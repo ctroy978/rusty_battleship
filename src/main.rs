bf57a768f2df7e1ce48c3774a67044bf8f222983
ctroy978 <ctroy978@gmail.com>
2021-02-28 07:02:28 -0800

    added ship size and orientation

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,30 @@ fn main() {
     //2d array &str
     let mut ocean = boards::Board::new(20, 20);
     
-    let mut player1: Vec<ships::Ship> = Vec::new();
-    let mut player2: Vec<ships::Ship> = Vec::new();
+    let mut fleets: Vec<ships::Ship> = Vec::new();
 
-    let mut total_ships = 5;
+    let mut total_ships = 6;
     
     while total_ships > 0{
 
         ocean.draw();
+        let fleet = 1;
+        let length = ships::Ship::valid_len(&mut fleets);
         let row = ocean.valid_row(); 
         let col = ocean.valid_col();
-        let length = 5;
-        let direction = ships::Direction::WestEast;
+        let direction = ships::Ship::valid_dir(); 
 
-        let ship = match ships::Ship::build_ship(&mut ocean, (row, col), length, direction){
+        match ships::Ship::build_ship(&mut ocean, fleet, (row, col), length, direction){
             Some(s) => {
                 println!("Ship placed in fleet");
                 ocean.place_ship(&s);
-                player1.push(s);
+                fleets.push(s);
             },
             None => {println!("Problem with ship. It was not added to fleet.")},
         };
         total_ships -= 1;
     }
 
-    println!("{:?}", player1);
+    println!("{:?}", fleets);
     ocean.draw();
 }
--- src/ships.rs
+++ src/ships.rs
@@ -1,16 +1,19 @@
+use std::io;
 
 use super::boards; 
 
 #[derive(Debug, PartialEq)]
 pub struct Ship {
+    pub fleet: usize,
     pub start: (String, usize),
     pub length: usize,
     pub direction: Direction,
 }
 
 impl Ship {
-    pub fn new(start: (String, usize), length: usize, direction: Direction) -> Self{
+    pub fn new(fleet: usize, start: (String, usize), length: usize, direction: Direction) -> Self{
         Self {
+            fleet,
             start,
             length,
             direction,
@@ -18,10 +21,11 @@ impl Ship {
     }
 
 
-pub fn build_ship(ocean: &mut boards::Board, start: (String, usize), 
+    ///Checks if ship fits in ocean before adding to fleet
+    pub fn build_ship(ocean: &mut boards::Board, fleet: usize, start: (String, usize), 
                    length: usize, direction: Direction ) -> Option<Ship>{
 
-        let ship = Ship::new((start.0, start.1), length, direction); 
+        let ship = Ship::new(fleet, (start.0, start.1), length, direction); 
 
         if ocean.fit_check(&ship){
             return Some(ship);
@@ -29,6 +33,63 @@ pub fn build_ship(ocean: &mut boards::Board, start: (String, usize),
             return None;
         }
 
+    }
+
+    ///Validates user input on ship type
+    pub fn valid_len(fleets: &mut Vec<Ship>) -> usize{
+
+        let carrier: usize = 8;
+        let battleship: usize = 7;
+        let destroyer: usize = 5;
+        let cruiser: usize = 4;
+        let pt_boat: usize = 2;
+
+        let ship_size = match fleets.len(){
+            0 => carrier,
+            1 => battleship,
+            2 => battleship,
+            3 => destroyer,
+            4 => cruiser,
+            _ => pt_boat,
+        };
+        let ship_type = match ship_size{
+            8 => "Carrier -len: 8",
+            7 => "Battleship -len: 7",
+            5 => "Destroyer -len: 5",
+            4 => "Cruiser -len: 3",
+            2 => "PT Boat -len: 2",
+            _ => "unknown",
+        };
+        println!("Prepare to place a {}", ship_type);
+        ship_size
+    }
+
+    ///validates user input for direction of ship
+    pub fn valid_dir() -> Direction{
+
+        loop{
+            let mut direction = String::new();
+            println!("Input ship orientation -NS or -EW");
+            io::stdin()
+                .read_line(&mut direction)
+                .expect("Issue reading direction input");
+
+            direction = direction.trim().to_uppercase().to_string();
+
+            let dir = match &direction[..]{
+                "NS" => Direction::NorthSouth, 
+                "-NS" => Direction::NorthSouth,
+                "NORTHSOUTH" => Direction::NorthSouth,
+                "EW" => Direction::WestEast,
+                "-EW" => Direction::WestEast,
+                "EASTWEST" => Direction::WestEast,
+                _ => {
+                    println!("Not a valid entry");
+                    continue;
+                }
+            }; 
+            return dir;
+        }
     }
 }
 
